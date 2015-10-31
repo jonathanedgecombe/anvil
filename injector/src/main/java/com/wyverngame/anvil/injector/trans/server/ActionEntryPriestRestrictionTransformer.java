932d32f2e501c4b9212b85fcd8ee74724d1b040c
Graham Edgecombe <gpe@grahamedgecombe.com>
2015-10-31 19:09:43 +0000

    Fix ActionEntry transformer.

--- injector/src/main/java/com/wyverngame/anvil/injector/trans/server/ActionEntryPriestRestrictionTransformer.java
+++ injector/src/main/java/com/wyverngame/anvil/injector/trans/server/ActionEntryPriestRestrictionTransformer.java
@@ -1,33 +1,52 @@
 package com.wyverngame.anvil.injector.trans.server;
 
+import java.util.Iterator;
+
+import com.google.common.collect.ImmutableSet;
 import com.wyverngame.anvil.injector.InjectorException;
-import com.wyverngame.anvil.injector.trans.ClassTransformer;
-import com.wyverngame.anvil.injector.util.AsmUtils;
-import org.checkerframework.checker.nullness.qual.Nullable;
+import com.wyverngame.anvil.injector.trans.MethodTransformer;
+import com.wyverngame.anvil.injector.util.InsnMatcher;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.FieldNode;
+import org.objectweb.asm.tree.FieldInsnNode;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.MethodNode;
 
-public final class ActionEntryPriestRestrictionTransformer extends ClassTransformer {
+public final class ActionEntryPriestRestrictionTransformer extends MethodTransformer {
+	private static final ImmutableSet<String> FIELDS = ImmutableSet.of(
+		"isAllowFo",
+		"isAllowFoOnSurface",
+		"isAllowVynora",
+		"isAllowMagranon",
+		"isAllowMagranonInCave",
+		"isAllowLibila",
+		"isAllowLibilaInCave"
+	);
+
 	public ActionEntryPriestRestrictionTransformer() {
-		super("com/wurmonline/server/behaviours/ActionEntry");
-	}
-
-	private void setFieldValue(ClassNode clazz, String field, String desc, Object value) {
-		@Nullable FieldNode node = AsmUtils.getField(clazz, field, desc);
-		if (node == null)
-			throw new InjectorException("Can't find field " + field);
-
-		node.value = value;
+		super("com/wurmonline/server/behaviours/ActionEntry", "<init>", "(SILjava/lang/String;Ljava/lang/String;[IIZ)V");
 	}
 
 	@Override
-	public void transform(ClassNode clazz) {
-		setFieldValue(clazz, "isAllowFo", "Z", 1);
-		setFieldValue(clazz, "isAllowFoOnSurface", "Z", 1);
-		setFieldValue(clazz, "isAllowVynora", "Z", 1);
-		setFieldValue(clazz, "isAllowMagranon", "Z", 1);
-		setFieldValue(clazz, "isAllowMagranonInCave", "Z", 1);
-		setFieldValue(clazz, "isAllowLibila", "Z", 1);
-		setFieldValue(clazz, "isAllowLibilaInCave", "Z", 1);
+	public void transform(ClassNode clazz, MethodNode method, InsnMatcher matcher) {
+		int patchedFields = 0;
+
+		for (Iterator<AbstractInsnNode[]> it = matcher.match("ALOAD ICONST_0 PUTFIELD"); it.hasNext();) {
+			AbstractInsnNode[] match = it.next();
+
+			FieldInsnNode putfield = (FieldInsnNode) match[2];
+			if (!putfield.owner.equals(clazz.name) || !putfield.desc.equals("Z"))
+				continue;
+
+			if (FIELDS.contains(putfield.name)) {
+				method.instructions.set(match[1], new InsnNode(Opcodes.ICONST_1));
+				patchedFields++;
+			}
+		}
+
+		if (patchedFields != FIELDS.size()) {
+			throw new InjectorException("Didn't patch the expected number of fields");
+		}
 	}
 }
