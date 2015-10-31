af97bf9696615bf35a2c94d47627380a1508b81d
Graham Edgecombe <gpe@grahamedgecombe.com>
2015-10-31 11:06:44 +0000

    Add logging to Injector.

--- injector/src/main/java/com/wyverngame/anvil/injector/Injector.java
+++ injector/src/main/java/com/wyverngame/anvil/injector/Injector.java
@@ -11,13 +11,19 @@ import com.wyverngame.anvil.injector.trans.client.WorldTickTransformer;
 import com.wyverngame.anvil.injector.trans.server.ChaosTransformer;
 import com.wyverngame.anvil.injector.trans.server.DiskIoTransformer;
 import com.wyverngame.anvil.injector.trans.server.FreedomAltarTransformer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class Injector {
+	private static final Logger logger = LoggerFactory.getLogger(Injector.class.getName());
+
 	public static void main(String[] args) throws IOException {
 		Injector.create().run();
 	}
 
 	public static Injector create() throws IOException {
+		logger.info("Reading jars...");
+
 		Module common = Module.read(Paths.get("api-common/lib/common.jar"));
 		Module client = Module.read(Paths.get("api-client/lib/client.jar"));
 		Module server = Module.read(Paths.get("api-server/lib/server.jar"));
@@ -44,10 +50,13 @@ public final class Injector {
 	}
 
 	public void run() throws IOException {
+		logger.info("Transforming classes...");
+
 		/* transform common.jar */
 		Application commonApplication = new Application(common);
 
 		for (Transformer transformer : commonTransformers) {
+			logger.info("Running transformer: {}...", transformer.getClass().getName());
 			transformer.transform(commonApplication);
 		}
 
@@ -55,6 +64,7 @@ public final class Injector {
 		Application clientApplication = new Application(common, client);
 
 		for (Transformer transformer : clientTransformers) {
+			logger.info("Running transformer: {}...", transformer.getClass().getName());
 			transformer.transform(clientApplication);
 		}
 
@@ -62,12 +72,15 @@ public final class Injector {
 		Application serverApplication = new Application(common, server);
 
 		for (Transformer transformer : serverTransformers) {
+			logger.info("Running transformer: {}...", transformer.getClass().getName());
 			transformer.transform(serverApplication);
 		}
 
 		ClassLoader dependencyClassLoader = getClass().getClassLoader(); // TODO load client/server libs here
 
 		/* write patched jars */
+		logger.info("Writing jars...");
+
 		client.write(clientApplication, dependencyClassLoader, Paths.get("api-client/lib/client-patched.jar"));
 		common.write(commonApplication, dependencyClassLoader, Paths.get("api-common/lib/common-patched.jar"));
 		server.write(serverApplication, dependencyClassLoader, Paths.get("api-server/lib/server-patched.jar"));
