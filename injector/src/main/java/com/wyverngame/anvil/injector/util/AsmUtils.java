3dcb3bbd01bfa849fb11b27a4b68912512ce3919
Graham Edgecombe <gpe@grahamedgecombe.com>
2015-10-31 12:52:47 +0000

    Annotate AsmUtils.

--- injector/src/main/java/com/wyverngame/anvil/injector/util/AsmUtils.java
+++ injector/src/main/java/com/wyverngame/anvil/injector/util/AsmUtils.java
@@ -1,10 +1,11 @@
 package com.wyverngame.anvil.injector.util;
 
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.objectweb.asm.tree.LocalVariableNode;
 import org.objectweb.asm.tree.MethodNode;
 
 public final class AsmUtils {
-	public static LocalVariableNode getLocalVariable(MethodNode method, String name, String desc) {
+	public static @Nullable LocalVariableNode getLocalVariable(MethodNode method, String name, String desc) {
 		for (LocalVariableNode var : method.localVariables) {
 			if (var.name.equals(name) && var.desc.equals(desc)) {
 				return var;
