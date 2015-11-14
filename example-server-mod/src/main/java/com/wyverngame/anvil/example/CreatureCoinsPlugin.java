6bd2d5f54abf67fd1eac7dc14fd0dd9d79aca3d1
Graham Edgecombe <gpe@grahamedgecombe.com>
2015-11-14 17:14:44 +0000

    Improve creature coins plugin.
    - Money is deducted from the king's money.
    
    - A random number of iron coins are spawned.
    
    - Monsters have more coins than normal animals.

--- example-server-mod/src/main/java/com/wyverngame/anvil/example/CreatureCoinsPlugin.java
+++ example-server-mod/src/main/java/com/wyverngame/anvil/example/CreatureCoinsPlugin.java
@@ -2,9 +2,10 @@ package com.wyverngame.anvil.example;
 
 import java.util.Random;
 
+import com.wurmonline.server.creatures.Creature;
+import com.wurmonline.server.economy.Economy;
+import com.wurmonline.server.economy.Shop;
 import com.wurmonline.server.items.Item;
-import com.wurmonline.server.items.ItemFactory;
-import com.wurmonline.server.items.ItemList;
 import com.wyverngame.anvil.api.Plugin;
 import com.wyverngame.anvil.api.PluginMetadata;
 import com.wyverngame.anvil.api.server.ServerPluginContext;
@@ -17,8 +18,28 @@ public final class CreatureCoinsPlugin extends Plugin<ServerPluginContext> {
 	@Override
 	public void init() {
 		on(CreateCreatureEvent.class, (ctx, evt) -> {
-			Item inventory = evt.getCreature().getInventory();
-			inventory.insertItem(ItemFactory.createItem(ItemList.coinGold, random.nextFloat() * 100, null));
+			Creature creature = evt.getCreature();
+			if (creature.isAnimal() || creature.isMonster()) {
+				/* generate random number of iron coins */
+				long iron = random.nextInt(creature.isMonster() ? 1000 : 200);
+
+				/* subtract from the king's iron */
+				Shop kingsMoney = Economy.getEconomy().getKingsShop();
+
+				long kingsIron = kingsMoney.getMoney();
+				if (iron > kingsIron) {
+					iron = kingsIron;
+				}
+
+				kingsIron -= iron;
+				kingsMoney.setMoney(kingsIron);
+
+				/* add to the creature's inventory */
+				Item inventory = creature.getInventory();
+				for (Item coin : Economy.getEconomy().getCoinsFor(iron)) {
+					inventory.insertItem(coin);
+				}
+			}
 		});
 	}
 }
