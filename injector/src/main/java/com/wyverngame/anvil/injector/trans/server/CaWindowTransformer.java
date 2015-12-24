3012257b45d4346ac6089c91199b83b1cff65d66
Graham Edgecombe <gpe@grahamedgecombe.com>
2015-12-24 17:48:44 +0000

    Fix CaWindowTransformer.

--- injector/src/main/java/com/wyverngame/anvil/injector/trans/server/CaWindowTransformer.java
+++ injector/src/main/java/com/wyverngame/anvil/injector/trans/server/CaWindowTransformer.java
@@ -2,41 +2,40 @@ package com.wyverngame.anvil.injector.trans.server;
 
 import java.util.Iterator;
 
-import com.wyverngame.anvil.injector.InjectorException;
+import com.google.common.collect.Iterators;
 import com.wyverngame.anvil.injector.trans.MethodTransformer;
 import com.wyverngame.anvil.injector.util.InsnMatcher;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.FieldInsnNode;
+import org.objectweb.asm.tree.InsnList;
 import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.VarInsnNode;
 
 public final class CaWindowTransformer extends MethodTransformer {
 	public CaWindowTransformer() {
-		super("com/wurmonline/server/players/PlayerInfo", "<init>", "(Ljava/lang/String;)V");
+		super("com/wurmonline/server/LoginHandler", "handleLogin", "(Ljava/lang/String;Ljava/lang/String;ZZZZLjava/lang/String;Ljava/lang/String;)V");
 	}
 
 	@Override
 	public void transform(ClassNode clazz, MethodNode method, InsnMatcher matcher) {
-		Iterator<AbstractInsnNode[]> it = matcher.match("ICONST_0 PUTFIELD", match -> {
-			FieldInsnNode putfield = (FieldInsnNode) match[1];
-			if (!putfield.owner.equals(clazz.name))
-				return false;
-
-			if (!putfield.name.equals("seesPlayerAssistantWindow"))
-				return false;
-
-			return putfield.desc.equals("Z");
+		Iterator<AbstractInsnNode[]> it = matcher.match("ALOAD ALOAD INVOKEVIRTUAL", match -> {
+			MethodInsnNode invoke = (MethodInsnNode) match[2];
+			return invoke.name.equals("setSaveFile") && invoke.owner.equals("com/wurmonline/server/players/Player");
 		});
 
-		if (!it.hasNext())
-			throw new InjectorException("failed to find injection point");
+		AbstractInsnNode[] match = Iterators.getOnlyElement(it);
 
-		AbstractInsnNode[] match = it.next();
-		method.instructions.set(match[0], new InsnNode(Opcodes.ICONST_1));
+		VarInsnNode file = (VarInsnNode) match[1];
 
-		if (it.hasNext())
-			throw new InjectorException("too many injection points");
+		InsnList list = new InsnList();
+		list.add(new VarInsnNode(Opcodes.ALOAD, file.var));
+		list.add(new InsnNode(Opcodes.ICONST_1));
+		list.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "com/wurmonline/server/players/PlayerInfo", "togglePlayerAssistantWindow", "(Z)Z", false));
+		list.add(new InsnNode(Opcodes.POP));
+
+		method.instructions.insert(match[2], list);
 	}
 }
